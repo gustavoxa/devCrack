c54ece16b636fda0b4816f5c23188325e03f489d
gustavoxa <xavier-at90@hotmail.com>
2023-12-09 12:07:17 -0500

    Cambios finales

--- EjerciciosGit/ejercicio.js
+++ EjerciciosGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let imagensaludar;
+    imagensaludar =  document.getElementById("imagenSaludar")
+    imagensaludar.src="./Imagenes/yagane-gane.gif"
     console.log("Ingresa a saludar")
 }
